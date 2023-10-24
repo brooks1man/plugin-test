24ce872aed0c1c8682cfe93f1f0a4d55d28df5df
Matt Brooks <mbrooks34@gsu.edu>
2023-10-24 10:48:13 -0400

    a

---
+++ plugin-test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: A Plugin Test
+ * Plugin URI: https://github.com/brooks1man/plugin-test
+ * Description: A WordPress plugin for testing.
+ * Version: 1.0.0
+ * Author: Matt Brooks
+ * License: none
+ * Update URI: https://github.com/brooks1man/plugin-test/blob/main/updates.json
+ */
+
+defined('ABSPATH') || exit;
